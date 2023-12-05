74a3c5db86950eb6030b76e8d5f3e1de33f67677
Abhi2282003 <140592089+Abhi2282003@users.noreply.github.com>
2023-12-05 19:41:22 +0530

    Update techstack.js

--- assets/js/techstack.js
+++ assets/js/techstack.js
@@ -3,25 +3,30 @@ AOS.init();
 const techStackCards = document.querySelector(".techstack-box");
 const techStack = [
   {
-    langImage: "assets/images/travel/trip1/pune.jpg",
+    langImage: "/pune.jpg",
     langName: "PUNE",
     langDesc: "<li> Registered Office: Tanish Orchid Phase 2, Charoli bk Pune- 412105 <li>Corporate Office: 1st Floor Chavan Niwas, Aundh-Ravet BRT Road,<br>Jagtap Dairy, Pune - 411027 <br>Phone: +91-9823939396</li>",
+    mapLink: "https://www.google.com/maps?q=Tanish+Orchid+Phase+2,Charoli+bk+Pune-412105"
   },
   {
-    langImage: "assets/images/travel/trip1/sangli.jpg",
+    langImage: "/sangli.jpg",
     langName: "Sangli",
     langDesc: "<li>Address<br>Phone no.</li>",
+    mapLink: "https://www.google.com/maps?q=Sangli"
   },
   {
-    langImage: "assets/images/travel/trip1/kolhapur.jpg",
+    langImage: "/kolhapur.jpg",
     langName: "Kolhapur",
     langDesc: "<li>Address <br> Phone no.</li>",
+    mapLink: "https://www.google.com/maps?q=Kolhapur"
   },
   {
-    langImage: "assets/images/travel/trip1/pandharpur.jpg",
+    langImage: "/pandharpur.jpg",
     langName: "Pandharpur",
     langDesc: "<li>Address <br> Phone no.</li>",
-  }];
+    mapLink: "https://www.google.com/maps?q=Pandharpur"
+  }
+];
 
 const displayTechStacksCards = () => {
   const entireCardTemplate = techStack.map((stack) => `
@@ -38,6 +43,7 @@ const displayTechStacksCards = () => {
                 </header>
                 <ul class="description">
                     ${stack.langDesc}
+                    <li><a href="${stack.mapLink}" target="_blank">View on Google Maps</a></li>
                 </ul>
             </div>
         </div>
